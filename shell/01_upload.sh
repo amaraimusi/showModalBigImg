30226433c785b235e156a7c13d4847de42a761e8
amaraimusi <amaraimusi@gmail.com>
2023-03-23 09:04:25 +0900

    shellまわり

---
+++ shell/01_upload.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz ../showModalBigImg amaraimusi@amaraimusi.sakura.ne.jp:www/example_js_k
+
+
+echo "------------ 送信完了"
+#cmd /k
---
+++ shell/smblic.bat
@@ -0,0 +1,1 @@
+cmd /c mklink /D C:\xampp\htdocs\sample_js_k\showModalBigImg C:\Users\user\git\showModalBigImg
